d2d2b591f95557b4a3e39b0f30d154bcb1a9ac73
JJRYY <kjr2436@gmail.com>
2021-05-24 13:12:00 +0900

    210524_spring

--- src/main/java/spring5_webmvc_study/controller/Member.java
+++ src/main/java/spring5_webmvc_study/controller/Member.java
@@ -65,6 +65,10 @@ public class Member {
 		}
 		this.password = newPassword;
 	}
+	
+	public boolean matchPassword(String password) {
+		return this.password.equals(password);
+	}
 
 	@Override
 	public String toString() {
